c29263dfbf54d9a72034a9d35850d21a261f5799
LeoCode12 <leonardo200043@gmail.com>
2020-04-11 22:33:42 -0500

    Project Modified 11-04-2020 2

--- PriceFlot/src/app/app-routing.module.ts
+++ PriceFlot/src/app/app-routing.module.ts
@@ -4,6 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then( m => m.ProductPageModule)
+  },
+  {
+    path: 'purchase',
+    loadChildren: () => import('./purchase/purchase.module').then( m => m.PurchasePageModule)
+  },
+  {
+    path: 'sale',
+    loadChildren: () => import('./sale/sale.module').then( m => m.SalePageModule)
+  },
+  {
+    path: 'summary',
+    loadChildren: () => import('./summary/summary.module').then( m => m.SummaryPageModule)
+  },
 ];
 
 @NgModule({
